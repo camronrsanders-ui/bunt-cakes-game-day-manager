6b54d8c120be70d8e621db6a707b2e5ea8c0d109
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-08-19 17:01:35 -0400

    Refresh cache for officiating tracker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='bunt-cakes-v2';
+const CACHE='bunt-cakes-v3';
 const ASSETS=['./','index.html','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
