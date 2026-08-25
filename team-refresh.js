042840412a9b710250b6e0d865042a4e85888a22
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-08-25 16:47:50 -0400

    Keep Player helper cache versions aligned

--- team-refresh.js
+++ team-refresh.js
@@ -166,7 +166,7 @@
     ['data-bunt-team-usability','/team-usability.js?v=3'],
     ['data-team-position-editor','/team-position-editor.js?v=2'],
     ['data-team-officiating-view','/team-officiating-view.js?v=1'],
-    ['data-team-role-badges','/team-role-badges.js?v=2'],
+    ['data-team-role-badges','/team-role-badges.js?v=1'],
     ['data-bunt-preferred-names','/preferred-names.js?v=2'],
     ['data-bunt-attendance','/team-attendance.js?v=5'],
     ['data-bunt-access-checkin','/team-access-checkin.js?v=4'],
