96b9f9c41a5afb887f8f776d9d3f5ef48d217ced
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-09-14 15:21:47 -0400

    Bump FeildHaus cache for player view fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='feildhaus-pilot-v1';
+const CACHE='feildhaus-pilot-v2';
 const CORE=['/manifest.webmanifest','/feildhaus-mark.svg','/generic-team-icon.svg'];
 
 self.addEventListener('install',event=>{
