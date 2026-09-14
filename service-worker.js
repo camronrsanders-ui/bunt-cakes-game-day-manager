e61d860340f97feac25e23c79979dad4b5140b98
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-09-14 16:26:52 -0400

    Bump FeildHaus cache after platform entry-point change

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='feildhaus-pilot-v3';
+const CACHE='feildhaus-pilot-v4';
 const CORE=['/manifest.webmanifest','/feildhaus-mark.svg','/generic-team-icon.svg'];
 
 self.addEventListener('install',event=>{
